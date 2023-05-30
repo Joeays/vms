cf961f81c05628db0bfaaa884a8c3579b7713a19
Joe <>
2023-05-30 16:08:21 +0800

    feat: 添加文件

---
+++ src/router/modules/sysRouter.js
@@ -0,0 +1,40 @@
+const sysRouter = {
+    path: '/sys',
+    meta: {
+        title: 'SYS',
+        icon: 'system',
+        show: true,
+    },
+    component: (resolve) => require(['@/views/Home.vue'], resolve),
+    redirect: '/sys/http',
+    children: [
+        {
+            path: 'http',
+            meta: {
+                title: 'HTTP',
+                icon: 'Laptop',
+                show: true,
+            },
+            component: (resolve) => require(['@/views/http/index.vue'], resolve)
+        },
+        {
+            path: 'arp',
+            meta: {
+                title: 'ARP',
+                icon: 'random',
+                show: true,
+            },
+            component: (resolve) => require(['@/views/arp/index.vue'], resolve)
+        },
+        {
+            path: 'dns',
+            meta: {
+                title: 'DNS',
+                icon: 'database',
+                show: true,
+            },
+            component: (resolve) => require(['@/views/dns/index.vue'], resolve)
+        }
+    ]
+}
+export default sysRouter
